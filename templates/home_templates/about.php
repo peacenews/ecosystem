281ab91dabb6f53ffd3cb02ac12ee0eab38df3c4
cyborgasmic69 <cyborgasmic69@gmail.com>
2015-08-21 00:11:58 +0100

    image references and placeholders

--- templates/home_templates/about.php
+++ templates/home_templates/about.php
@@ -1,20 +1,30 @@
 <!--
-"Peace News Ecosystem" is a CMS developed to allow small groups with no tech' expertise to have an internet presence. Its USP is freedom from choice. You can see one installation of Peace News Ecosystem at https://zylum.org/
-Copyright (C) 2014 Zylum Ltd.
-admin@zylum.org / 5 Caledonian Rd, London, N1 9DY
-
-Version one of Peace News Ecosystem was authored by http://www.wave.coop/ info@wave.coop
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html
--->
+/**
+ * "Peace News Ecosystem" is a CMS developed to allow small groups with no tech' expertise
+ * to have an internet presence. Its USP is freedom from choice. You can see one installation
+ * of Peace News Ecosystem at https://zylum.org/
+ *
+ * Copyright (C) 2014 Zylum Ltd.
+ * admin@zylum.org / 5 Caledonian Rd, London, N1 9DY
+ *
+ * Version one of Peace News Ecosystem was authored by http://www.wave.coop/ info@wave.coop
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see http://www.gnu.org/licenses/agpl-3.0.html
+ */
+ -->
 
 <div class="row">
     <div class="large-3 small-6 medium-3 columns logo">
-        <a href="/"><img src="/../img/logo.png" id="logo" /></a>
+        <a href="/"><img src="/img/logo.png" id="logo" /></a>
     </div>
     <div class="large-9 small-6 medium-9 columns">
         <nav class="top-bar" data-topbar role="navigation">
@@ -83,7 +93,7 @@ You should have received a copy of the GNU Affero General Public License along w
             <p>Body text</p>
 
             <div class="leaf-bg text-center">
-                <img src="backbround.jpg"> <!-- Background image -->
+                <img src="/img/background.png"> <!-- Background image -->
             </div><!--leaf-bg text-center-->
             <div class="back-to-top text-center">
                 <div class="text-center"><img src="/img/back-to-top.png"><!-- Back to top image --></div><!--text-center-->
